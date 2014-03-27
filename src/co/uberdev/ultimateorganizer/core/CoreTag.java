4ac84f1e13e8a9fb18f6c2f6ae4ab8dbd174c695
Dunku Coder <boyinankara@gmail.com>
2014-03-27 16:44:34 +0200

    CoreTag class created getter setter methods and properties #1

---
+++ src/co/uberdev/ultimateorganizer/core/CoreTag.java
@@ -0,0 +1,47 @@
+package co.uberdev.ultimateorganizer.core;
+
+/**
+ * Created by mozart on 27/03/14.
+ */
+public class CoreTag extends Core
+{
+    protected String name;
+    protected int colour;
+    protected long id;
+    protected long ownerId;
+
+    public CoreTag(String name)
+    {
+        this.name = name;
+    }
+
+    public int getColour()
+    {
+        return colour;
+    }
+
+    public void setColour(int colour)
+    {
+        this.colour = colour;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public long getOwnerId()
+    {
+        return ownerId;
+    }
+
+    public void setOwnerId(long ownerId)
+    {
+        this.ownerId = ownerId;
+    }
+}
