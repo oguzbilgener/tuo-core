52737e355b6e8c3e65dbb3bdf6f5ee633c50d435
Oguz Bilgener <oguzbilgener@gmail.com>
2014-04-16 17:30:39 +0300

    generator utils

--- src/co/uberdev/ultimateorganizer/core/CoreUtils.java
+++ src/co/uberdev/ultimateorganizer/core/CoreUtils.java
@@ -9,4 +9,30 @@ public class CoreUtils
     {
         return (int) (System.currentTimeMillis()/1000);
     }
+
+    public static String generatePublicKey(String emailAddress, String salt)
+    {
+        return CoreCrypto.sha1(salt + emailAddress);
+    }
+
+    public static String generateSecretToken( String emailAddress, String hashedPassword, String secretSalt)
+    {
+        return CoreCrypto.sha1( secretSalt + randomAlphanumericString(10) + emailAddress + hashedPassword);
+    }
+
+    public static int randomNumber(int a, int b)
+    {
+        return a + (int)(Math.random()*(b-a));
+    }
+
+    public static String randomAlphanumericString(int length)
+    {
+        String set = "abcdefghijklmnopqrstuwxyz0123456789";
+        StringBuilder builder = new StringBuilder();
+        for(int i=0; i < length; i++)
+        {
+            builder.append(set.charAt( randomNumber(0, set.length()) ));
+        }
+        return builder.toString();
+    }
 }
