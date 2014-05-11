367eae5535f7c7721bc4df32a1b29d6d2aa7b708
Oguz Bilgener <oguzbilgener@gmail.com>
2014-05-12 02:45:51 +0300

    obligatory inheritance change

--- src/co/uberdev/ultimateorganizer/core/CoreTags.java
+++ src/co/uberdev/ultimateorganizer/core/CoreTags.java
@@ -1,9 +1,13 @@
 package co.uberdev.ultimateorganizer.core;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
 /**
  * Created by oguzbilgener on 30/04/14.
  */
-public class CoreTags extends CoreList
+public class CoreTags extends ArrayList<CoreTag>
 {
 	public CoreTag get(int i)
 	{
@@ -13,5 +17,30 @@ public class CoreTags extends CoreList
 	public Core get(int i, boolean b)
 	{
 		return super.get(i);
+	}
+
+	public String asJsonString()
+	{
+		return new Gson().toJson(this);
+	}
+
+	public static <T> T fromJson(String json, Class<T> type)
+	{
+		return new Gson().fromJson(json, type);
+	}
+
+	public String toString()
+	{
+		return asJsonString();
+	}
+
+	public static CoreList fromArrayList(ArrayList<Core> coreArrayList)
+	{
+		CoreList list = new CoreList();
+		for(int i=0; i<coreArrayList.size(); i++)
+		{
+			list.set(i, coreArrayList.get(i));
+		}
+		return list;
 	}
 }
