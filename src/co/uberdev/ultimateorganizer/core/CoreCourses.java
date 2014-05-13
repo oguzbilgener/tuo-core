56981a1d96464dca54d6f149641930de6303e20c
Oguz Bilgener <oguzbilgener@gmail.com>
2014-05-13 11:04:33 +0300

    CoreCourses inheritance change

--- src/co/uberdev/ultimateorganizer/core/CoreCourses.java
+++ src/co/uberdev/ultimateorganizer/core/CoreCourses.java
@@ -1,12 +1,26 @@
 package co.uberdev.ultimateorganizer.core;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
 /**
  * Created by oguzbilgener on 14/04/14.
  */
-public class CoreCourses extends CoreList
+public class CoreCourses extends ArrayList<CoreCourse>
 {
-    public CoreCourse get(int i)
-    {
-        return (CoreCourse) super.get(i);
-    }
+	public String asJsonString()
+	{
+		return new Gson().toJson(this);
+	}
+
+	public static <T> T fromJson(String json, Class<T> type)
+	{
+		return new Gson().fromJson(json, type);
+	}
+
+	public String toString()
+	{
+		return asJsonString();
+	}
 }
